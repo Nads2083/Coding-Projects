0a6f9f63089863297800f2e2f9c972b319c4dd71
Riaan Schuld <rhschuld@gmail.com>
2023-04-01 20:29:10 +0000

    Just a little bit more logic in main and 2 simple functions to test

--- RiaanCalc.py
+++ RiaanCalc.py
@@ -7,8 +7,15 @@ def main(): #Controls the flow of the program (calls the other modules). This in
         if action not in range(1,7):
             print ('thank you come again')
             break
-        else:
-            print(f'do maths with vars action#:{action}, var1#:{value1}, var2#:{value2}') 
+        #else:
+        #    print(f'do maths with vars action#:{action}, var1#:{value1}, var2#:{value2}') 
+        if action == 1:
+            operator="+"
+            result=add(value1,value2)
+        if action == 2:
+            operator="-"
+            result=subtract(value1,value2)
+        userOutput(value1,value2,result,operator)
 
 
 
@@ -41,8 +48,6 @@ def userInput(): #Asks the user to select a menu option. If any menu item is sel
         return(validMenuChoice,False,False)
     
 
-
-
 def inputTest(inputVar): #Accepts one value from userInput(). Tests value to make sure it's a number. If it's not a number, it will ask for a valid number. This is one of the few times you can have a user input values in a function other than "userInput(). Returns valid numbers.
     while type(inputVar) != int:
             try:
@@ -56,14 +61,15 @@ def inputTest(inputVar): #Accepts one value from userInput(). Tests value to mak
 
 
 def add(validCompNumber1,validCompNumber2): #Accepts two numbers, returns the sum
-    pass
+    addFunctionresult=validCompNumber1+validCompNumber2
+    return addFunctionresult
 
 
 
 
 def subtract(validCompNumber1,validCompNumber2): #Accepts two numbers, returns the difference of the first number minus the second number
-    pass
-
+    subtractFunctionResult=validCompNumber1-validCompNumber2
+    return subtractFunctionResult
 
 
 
@@ -95,8 +101,8 @@ def exponent(validCompNumber1,validCompNumber2): #Accepts two numbers, returns t
 
 
 
-def userOutput(validCompNumber1,validCompNumber2): #Gives a user friendly output of the selected calculation. HINT: make your code reusable so that this method contains no decision structure!
+def userOutput(value1,value2,result,operator): #Gives a user friendly output of the selected calculation. HINT: make your code reusable so that this method contains no decision structure!
     pass
-
+    print(f'{value1} {operator} {value2} = {result}')
 
 main()
